16e5e31ecdc3dde78af45763fea6c5869b9be668
Shiva Pranita <ambatishivapranita@gmail.com>
2025-01-18 21:46:22 +0530

    modified reverse_array

--- arrays/reverse_array.cpp
+++ arrays/reverse_array.cpp
@@ -1,22 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    /*brute force way: 
-    -> create another array of same size and itreate through the array from backwards
-    -> and push it to the new array and then iterate through new array 
-    -> and replace the org array contents.
-    TC : O(n)[for iterating once in org array] + O(n)[for iterating in the new array] + O(n)[for iterating through org array]
-    total TC = O(3n)
-    SC : O(n) where n is the size of an array
-    */
-
-   /*optimal : 
-   -> iterate through the array once by keeping 2 pointers one at 0 index and one at n-1 index
-   -> swap both and after every swap increment the pointer at start  and decrement the pointer at end
-   -> until start > end.
-   TC : O(n)
-   SC : O(1) - no extra space is used except the input array space.
-   */
   int size;
   cin >> size;
   int arr[100000];
@@ -40,3 +24,19 @@ int main(){
 
     return 0;
 }
+    /*brute force way: 
+    -> create another array of same size and itreate through the array from backwards
+    -> and push it to the new array and then iterate through new array 
+    -> and replace the org array contents.
+    TC : O(n)[for iterating once in org array] + O(n)[for iterating in the new array] + O(n)[for iterating through org array]
+    total TC = O(3n)
+    SC : O(n) where n is the size of an array
+    */
+
+   /*optimal : 
+   -> iterate through the array once by keeping 2 pointers one at 0 index and one at n-1 index
+   -> swap both and after every swap increment the pointer at start  and decrement the pointer at end
+   -> until start > end.
+   TC : O(n)
+   SC : O(1) - no extra space is used except the input array space.
+   */
